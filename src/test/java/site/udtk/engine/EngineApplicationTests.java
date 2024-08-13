eca154f18f60f52f2033c4a4ade446fb5ee7f0fb
takealook97 <takealook97@naver.com>
2024-08-14 05:01:50 +0900

    chore: build init

---
+++ src/test/java/site/udtk/engine/EngineApplicationTests.java
@@ -0,0 +1,13 @@
+package site.udtk.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
