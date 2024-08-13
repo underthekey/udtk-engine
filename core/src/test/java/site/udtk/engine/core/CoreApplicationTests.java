30d5e97f3568d07faccb298153ebcc31d8deb759
takealook97 <takealook97@naver.com>
2024-08-14 08:44:37 +0900

    chore: add config-server module

---
+++ core/src/test/java/site/udtk/engine/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package site.udtk.engine.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
