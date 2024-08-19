6bf7df1d43894330e8e677517fc138de4eac43cc
takealook97 <takealook97@naver.com>
2024-08-20 02:29:58 +0900

    chore: set security config and user config class to set eureka-server auth

---
+++ eureka-server/src/main/java/site/udtk/eurekaserver/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package site.udtk.eurekaserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+	private final UserConfig userConfig;
+
+	@Bean
+	BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable);
+
+		http
+			.authorizeHttpRequests((auth) -> auth.anyRequest().authenticated());
+
+		http
+			.httpBasic(Customizer.withDefaults());
+
+		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+
+		UserDetails user1 = User.builder()
+			.username(userConfig.getUsername())
+			.password(bCryptPasswordEncoder().encode(userConfig.getPassword()))
+			.roles(userConfig.getRole())
+			.build();
+
+		return new InMemoryUserDetailsManager(user1);
+	}
+}
