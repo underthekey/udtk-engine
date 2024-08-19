fcb26bd3b993be2fde048603c9f11f330dfc8502
takealook97 <takealook97@naver.com>
2024-08-20 02:29:32 +0900

    chore: add @EnableConfigurationProperties annotation to set user details with class

--- eureka-server/src/main/java/site/udtk/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/site/udtk/eurekaserver/EurekaServerApplication.java
@@ -2,10 +2,14 @@ package site.udtk.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import site.udtk.eurekaserver.config.UserConfig;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigurationProperties(UserConfig.class)
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
