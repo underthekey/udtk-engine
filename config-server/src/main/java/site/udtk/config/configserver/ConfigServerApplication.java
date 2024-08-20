b50c994066fd5f8746e287d4a57bcb79bc0400ab
takealook97 <takealook97@naver.com>
2024-08-21 05:13:11 +0900

    chore: remove security config and user config
    separate mutual concern to module name "global-security"

--- config-server/src/main/java/site/udtk/config/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/site/udtk/config/configserver/ConfigServerApplication.java
@@ -2,15 +2,13 @@ package site.udtk.config.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-import site.udtk.config.configserver.config.UserConfig;
-
-@SpringBootApplication
+@SpringBootApplication(
+	scanBasePackages = "site.udtk.globalsecurity"
+)
 @EnableConfigServer
-@EnableConfigurationProperties(UserConfig.class)
 @EnableDiscoveryClient
 public class ConfigServerApplication {
 
--- eureka-server/src/main/java/site/udtk/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/site/udtk/eurekaserver/EurekaServerApplication.java
@@ -2,14 +2,12 @@ package site.udtk.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-import site.udtk.eurekaserver.config.UserConfig;
-
-@SpringBootApplication
+@SpringBootApplication(
+	scanBasePackages = "site.udtk.globalsecurity"
+)
 @EnableEurekaServer
-@EnableConfigurationProperties(UserConfig.class)
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
