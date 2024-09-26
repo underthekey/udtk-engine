a45812527cb9d8d4cfe5678e0cf9c1cbe346f807
takealook <takealook97@naver.com>
2024-09-26 19:00:42 +0900

    chore: package path updated

--- config-server/src/main/java/com/underthekey/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/underthekey/configserver/ConfigServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication(
-	scanBasePackages = "site.udtk.globalsecurity"
+	scanBasePackages = "com.underthekey.globalsecurity"
 )
 @EnableConfigServer
 @EnableDiscoveryClient
